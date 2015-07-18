2036d46a748f256a42b8c979020ed18444c1ef69
Philip James <pjj@philipjohnjames.com>
2015-07-17 21:52:03 -0700

    Prep the parser utils for the future

--- bockus/books/utils.py
+++ bockus/books/utils.py
@@ -12,7 +12,7 @@ MIMETYPES = (
 )
 
 
-def parse_folder(client, path):
+def parse_folder(client, path, user=None):
     print("working on {}".format(path))
     metadata = client.metadata(path)
     if not metadata.get('is_dir'):
@@ -20,32 +20,39 @@ def parse_folder(client, path):
     else: 
         for item in metadata.get('contents'):
             if not item.get('is_dir'):
-                parse_item(item)
+                parse_item(item, user)
             else:
                 parse_folder(client, item.get('path'))
 
 
-def parse_item(item):
+def parse_item(item, user=None):
     if item.get('mime_type') in MIMETYPES:
         filename = item.get('path').split('/')[-1]
         name, extension = os.path.splitext(filename)
         try:
+            # Do we already know about this file?
             bookfile = BookFileVersion.objects.get(
                 path=item.get('path'),
                 filetype=extension[1:],
             )
         except BookFileVersion.DoesNotExist:
             try:
+                # If not, do we know about another file with the same
+                # filename?
                 bookfile = BookFileVersion.objects.get(
                     path=item.get('path'),
                 )
+                # If so, create a new file with the new file type
                 new_bookfile = BookFileVersion.objects.create(
                     path=item.get('path'),
                     filetype=extension[1:],
                     book=bookfile.book,
                     storage_provider='dropbox',
+                    meta=item,
                 )
             except BookFileVersion.DoesNotExist:
+                # Apparently you know nothing, Jon Snow
+                # Create a new book and book file.
                 book = Book.objects.create(
                     title=name,
                 )
@@ -54,4 +61,5 @@ def parse_item(item):
                     filetype=extension[1:],
                     book=book,
                     storage_provider='dropbox',
+                    meta=item,
                 )
