6ab7358e32f26dab0e6362d0ab5ec0dcd5b25554
Philip James <pjj@philipjohnjames.com>
2015-07-28 16:52:06 -0700

    Made updates to force script to work

--- scripts/update_haystack_index.sh
+++ scripts/update_haystack_index.sh
@@ -1,4 +1,6 @@
-source .bashrc
+#! /bin/bash
+source /home/deploy/.bashrc
+source `which virtualenvwrapper.sh`
 workon booksonas
 cd bockus
 python manage.py update_index --age=1 --settings=bockus.prod_settings
