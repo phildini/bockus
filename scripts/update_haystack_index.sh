2b7560788c986713a41224ca5bc84de9c7b95d81
Philip James <pjj@philipjohnjames.com>
2015-08-30 13:18:41 -0700

    Remove old items from search index

--- scripts/update_haystack_index.sh
+++ scripts/update_haystack_index.sh
@@ -3,5 +3,5 @@ source /home/deploy/.bashrc
 cd /home/deploy/Env/booksonas
 source bin/activate
 cd /home/deploy/bockus
-python manage.py update_index --age=1 --settings=bockus.prod_settings
+python manage.py update_index --remove --age=1 --settings=bockus.prod_settings
 deactivate
